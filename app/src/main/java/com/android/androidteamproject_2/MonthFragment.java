9a555e9682d95b3276025b04ddc5ec06e2fb688b
ilyunjung <101690649+ilyunjung@users.noreply.github.com>
2022-05-10 19:34:10 +0900

    .

--- app/src/main/java/com/android/androidteamproject_2/MonthFragment.java
+++ app/src/main/java/com/android/androidteamproject_2/MonthFragment.java
@@ -28,6 +28,7 @@ public class MonthFragment extends Fragment {
     private static final String ARG_PARAM2 = "month";
     private int year;
     private int month;
+    int h;
     static GridAdapter adapter;
     private Calendar mCalendar;
     int firstday; // 첫날의 요일
@@ -53,6 +54,9 @@ public class MonthFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_month_view, container, false);
         GridView gridview = (GridView) v.findViewById(R.id.gridview);
         setCalendar(year, month); //daylist를 초기화 히면서 year, month 정보를 넣는다.
+        //linearlayout의 크기를 구해 textview에 맞게 설정
+        LinearLayout ll = (LinearLayout) v.findViewById(R.id.calendar_layout);
+        h=ll.getHeight();
 
         daylist = new ArrayList<String>();
         mCalendar.set(Calendar.YEAR,year);
@@ -101,4 +105,66 @@ public class MonthFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-}
+    class GridAdapter extends BaseAdapter {
+        private final List<String> list;
+        private final LayoutInflater inflater;
+
+        public GridAdapter(Context context, List<String> list) {
+            //입력받음 daylist로 객체 초기화
+            this.list = list;
+            this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {//리스트 크기 반환
+            return list.size();
+        }
+
+        @Override
+        public String getItem(int position) {//리스트 해탕 위치에 날짜 반환
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {//리스트의 위치 반환
+            return position;
+        }
+
+        @Override
+        //각각 포지션에 convertview 설정
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            if (convertView == null) {// convertView는 그리드 하나
+                convertView = inflater.inflate(R.layout.calendar_gridview, parent, false);
+                // calendar_gridview.xml파일을 convertview 객체로 반환
+                holder = new ViewHolder();// View를 담고 있는 ViewHolder 객체
+                holder.tvItemGridView = (TextView) convertView.findViewById(R.id.tv_item_gridview);
+                // holder의 TextView객체에 calendar_gridview.xml의 텍스트뷰를 연결한다
+                convertView.setTag(holder);
+                // convertView하나마다의 태그에 둘을 연결하기위해 태그를 holder로 설정.
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+                //convertview의 태그를 가져와 holder에 초기화한다. 따라서 holder는 convertview의 태그가 된다.
+            }
+            holder.tvItemGridView.setText("" + getItem(position)); //그리드뷰 각 포지션에 날짜 입력
+            //--------------글자 색 설정---------------
+            int color = Color.BLACK;
+            switch (position % 7) {
+                case 0: // 일요일은 빨강색으로 설정
+                    color = Color.RED;
+                    break;
+                case 6: // 토요일은 파랑으로 설정
+                    color = Color.BLUE;
+                    break;
+            }
+            holder.tvItemGridView.setHeight(h / 6);
+            holder.tvItemGridView.setTextColor(color);
+            //그리드뷰에 적힌 날짜 색 설정
+            return convertView;
+        }
+    }
+    public class ViewHolder {// ViewHolder 클래스(태그에 쓰일 클래스)
+        TextView tvItemGridView;
+    }
+    }
--- app/src/main/java/com/android/androidteamproject_2/ViewHolder.java
+++ app/src/main/java/com/android/androidteamproject_2/ViewHolder.java
@@ -2,6 +2,3 @@ package com.android.androidteamproject_2;
 
 import android.widget.TextView;
 
-public class ViewHolder {// ViewHolder 클래스(태그에 쓰일 클래스)
-    TextView tvItemGridView;
-}
